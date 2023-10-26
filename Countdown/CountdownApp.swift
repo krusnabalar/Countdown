d4e034007906a197c43954b558ee5bfe244145b1
krusnabalar <90931286+krusnabalar@users.noreply.github.com>
2023-10-25 21:24:09 -0700

    Initial Commit

---
+++ Countdown/CountdownApp.swift
@@ -0,0 +1,17 @@
+//
+//  CountdownApp.swift
+//  Countdown
+//
+//  Created by Krusna Balar on 2023-10-25.
+//
+
+import SwiftUI
+
+@main
+struct CountdownApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
